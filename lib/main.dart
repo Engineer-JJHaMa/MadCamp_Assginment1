b27eb80839856395a8245bb666882a28f4c2d67a
YinaePark <dlsoso1@unist.ac.kr>
2023-06-30 00:40:35 +0900

    feat: navigation bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,35 @@
+import 'package:assignment1/phone_nums.dart' as phone_nums;
+import 'package:assignment1/gallery.dart' as gallery;
+import 'package:assignment1/tmp_tab.dart' as tmp_tab;
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
         appBar: AppBar(),
-        body: Container(),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            color: Colors.blue,
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          ),
+        body: TabBarView(
+          children: const [
+            phone_nums.PhoneNums(),
+            gallery.Gallery(),
+            tmp_tab.TmpTab(),
+          ],
         ),
-      )
-    );
+        bottomNavigationBar: Container(
+            color: Colors.blue,
+            child: TabBar(tabs: const [
+              Tab(icon: Icon(Icons.phone)),
+              Tab(icon: Icon(Icons.perm_media)),
+              Tab(icon: Icon(Icons.contact_page)),
+            ])),
+      ),
+    ));
   }
 }
