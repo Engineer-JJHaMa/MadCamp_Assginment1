9f27ece43b07067b7a17a0039e1b1be379c46e95
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-06-30 16:45:55 +0900

    feat: 추가된 사진이 프레임에 가득 차도록 수정

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
@@ -20,54 +19,18 @@ class _GalleryState extends State<Gallery> {
         backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
       ),
-      body: Container(
-        child: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(2),
-          crossAxisSpacing: 2,
-          mainAxisSpacing: 2,
-          crossAxisCount: 3,
-          children: _pickedImgs.map((xf) => Container(
-            child: Image.file(File(xf.path)),
-          )).toList(),
-          // children: <Widget>[
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[100],
-          //     child: const Text("He'd have you all unravel at the"),
-          //   ),
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[200],
-          //     child: const Text('Heed not the rabble'),
-          //   ),
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[300],
-          //     child: const Text('Sound of screams but the'),
-          //   ),
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[400],
-          //     child: const Text('Who scream'),
-          //   ),
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[500],
-          //     child: const Text('Revolution is coming...'),
-          //   ),
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[600],
-          //     child: const Text('Revolution, they...'),
-          //   ),
-          //   Container(
-          //     padding: const EdgeInsets.all(8),
-          //     color: Colors.teal[600],
-          //     child: const Text('Revolution, they...'),
-          //   ),
-          // ],
-        ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(2),
+        crossAxisSpacing: 2,
+        mainAxisSpacing: 2,
+        crossAxisCount: 3,
+        children: _pickedImgs.map((xf) => Container(
+          child: Image.file(
+            File(xf.path),
+            fit: BoxFit.cover,
+            ),
+        )).toList(),
       ),
     );
   }
