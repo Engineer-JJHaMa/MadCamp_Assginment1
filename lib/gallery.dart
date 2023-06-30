d03c9fa55c69966fe0faab86aa2b8f0438703895
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-06-30 15:29:33 +0900

    feat: 플로팅 버튼 클릭시 갤러리에서 게시할 사진 선택 가능

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
-class Gallery extends StatelessWidget {
+
+class Gallery extends StatefulWidget {
   const Gallery({Key? key}) : super(key: key);
 
   @override
+  State<Gallery> createState() => _GalleryState();
+}
+
+class _GalleryState extends State<Gallery> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 229, 235, 237),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){_pickImg();},
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
       body: Container(
         child: GridView.count(
           primary: false,
@@ -14,71 +27,77 @@ class Gallery extends StatelessWidget {
           crossAxisSpacing: 2,
           mainAxisSpacing: 2,
           crossAxisCount: 3,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[100],
-              child: const Text("He'd have you all unravel at the"),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[200],
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[300],
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[400],
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[500],
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.teal[600],
-              child: const Text('Revolution, they...'),
-            ),
-          ],
+          children: _pickedImgs.map((xf) => Container(
+            child: Image.file(File(xf.path)),
+          )).toList(),
+          // children: <Widget>[
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[100],
+          //     child: const Text("He'd have you all unravel at the"),
+          //   ),
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[200],
+          //     child: const Text('Heed not the rabble'),
+          //   ),
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[300],
+          //     child: const Text('Sound of screams but the'),
+          //   ),
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[400],
+          //     child: const Text('Who scream'),
+          //   ),
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[500],
+          //     child: const Text('Revolution is coming...'),
+          //   ),
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[600],
+          //     child: const Text('Revolution, they...'),
+          //   ),
+          //   Container(
+          //     padding: const EdgeInsets.all(8),
+          //     color: Colors.teal[600],
+          //     child: const Text('Revolution, they...'),
+          //   ),
+          // ],
         ),
       ),
     );
+  }
+
+  // getPermission() async{
+  //   var status = await Permission.camera.status;
+  //   if(status.isGranted){
+  //     print('허락됨');
+  //   } else if (status.isDenied){
+  //     print('거절됨');
+  //     Permission.contacts.request(); // 허락해달라고 팝업띄우는 코드
+  //   }
+  // }
+
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   // getPermission();
+  // }
+
+  final ImagePicker _picker = ImagePicker();
+  List<XFile> _pickedImgs = [];
+
+  Future<void> _pickImg() async {
+    final List<XFile>? images = await _picker.pickMultiImage();
+    if(images != null) {
+      setState(() {
+        _pickedImgs = images;
+      });
+    }
   }
 }
 
