1171a0b244242d8055b8d41c7e829a24e5af06ed
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-06-30 12:02:51 +0900

    feat: set Gallery class

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -5,6 +5,80 @@ class Gallery extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.lightBlue, body: Center());
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 229, 235, 237),
+      body: Container(
+        child: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(2),
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
+          crossAxisCount: 3,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[100],
+              child: const Text("He'd have you all unravel at the"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[200],
+              child: const Text('Heed not the rabble'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[300],
+              child: const Text('Sound of screams but the'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[400],
+              child: const Text('Who scream'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[500],
+              child: const Text('Revolution is coming...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.teal[600],
+              child: const Text('Revolution, they...'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
