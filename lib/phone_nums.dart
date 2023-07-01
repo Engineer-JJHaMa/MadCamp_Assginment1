bd56c087133a8f973b6bab009e573f6ad2d38003
YinaePark <dlsoso1@unist.ac.kr>
2023-07-01 16:51:27 +0900

    feat: add modal and call function

--- lib/phone_nums.dart
+++ lib/phone_nums.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
-// import 'package:contacts_service/contacts_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:fast_contacts/fast_contacts.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+
 import 'package:flutter/foundation.dart';
 
 class PhoneNums extends StatefulWidget {
   PhoneNums({Key? key}) : super(key: key);
+
+
 
   @override
   _PhoneNums createState() => _PhoneNums(); // State 생성.
 }
 
 class _PhoneNums extends State<PhoneNums> {
+  @override
+  void initState() {
+    super.initState();
+    getPhones();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,6 +32,7 @@ class _PhoneNums extends State<PhoneNums> {
           height: double.infinity,
           child: FutureBuilder(
             future: getContacts(),
+
             builder: (context, AsyncSnapshot snapshot) {
               if (snapshot.data == null) {
                 return const Center(
@@ -46,6 +57,73 @@ class _PhoneNums extends State<PhoneNums> {
                             Text((contact.phones).toString()),
                           ],
                         ),
+                        onTap: () async {
+                          await showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Center(
+                                    child: SizedBox(
+                                      height:240,
+                                      child: Scaffold(
+                                        appBar: AppBar(
+                                          leading: const CircleAvatar(
+                                            radius: 20,
+                                            child: Icon(Icons.person),
+                                          ),
+                                          title: Text(contact.displayName),
+                                        ),
+                                        body: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: <Widget>[
+                                                MaterialButton(
+                                                  onPressed: () {
+                                                    makePhoneCall((contact.phones).toString());
+                                                    },
+                                                  shape: CircleBorder(),
+                                                  child: Icon(
+                                                    Icons.phone,
+                                                    size: 20,
+                                                  ),
+                                                  color: Colors.greenAccent,
+                                                  textColor: Colors.white,
+                                                ),
+                                                MaterialButton(
+                                                  onPressed: () {},
+                                                  shape: CircleBorder(),
+                                                  child: Icon(
+                                                    Icons.message,
+                                                    size: 20,
+                                                  ),
+                                                  color: Colors.greenAccent,
+                                                  textColor: Colors.white,
+                                                ),
+                                                MaterialButton(
+                                                  onPressed: () {},
+                                                  shape: CircleBorder(),
+                                                  child: Icon(
+                                                    Icons.videocam,
+                                                    size: 20,
+                                                  ),
+                                                  color: Colors.greenAccent,
+                                                  textColor: Colors.white,
+                                                ),
+                                                MaterialButton(
+                                                  onPressed: () {},
+                                                  shape: CircleBorder(),
+                                                  child: Icon(
+                                                    Icons.info,
+                                                    size: 20,
+                                                  ),
+                                                  color: Colors.greenAccent,
+                                                  textColor: Colors.white,
+                                            ),
+                                          ],
+                                      ),
+                                    ),
+                                  ),
+                                  );
+                              });
+                        },
                       ),
                       const Divider()
                     ]);
@@ -59,6 +137,7 @@ class _PhoneNums extends State<PhoneNums> {
 
   Future<List<Contact>> getContacts() async {
     bool isGranted = await Permission.contacts.status.isGranted;
+
     if (!isGranted) {
       isGranted = await Permission.contacts.request().isGranted;
     }
@@ -67,4 +146,22 @@ class _PhoneNums extends State<PhoneNums> {
     }
     return [];
   }
+  getPhones() async {
+    bool isGranted = await Permission.phone.status.isGranted;
+    if (!isGranted) {
+      isGranted = await Permission.phone.request().isGranted;
+    }
+    return [];
+  }
+
+  void makePhoneCall(String url) async {
+    String parsedUrl = url.substring(1, url.length - 1);
+    if (await canLaunchUrlString('tel:$parsedUrl')) {
+      await launchUrlString('tel:$parsedUrl');
+    }
+    else {
+      throw 'cannot call';
+    }
+  }
+
 }
