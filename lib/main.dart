358a8509b30bd506fdceb793123998992b9f640b
SeungJong Ha <hippo0419@kaist.ac.kr>
2023-07-03 21:07:27 +0900

    style: main.dart 들여쓰기 맞추기

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(),
-        body: TabBarView(
-          children: [
-            phone_nums.PhoneNums(),
-            gallery.Gallery(),
-            calendar.Calendar(),
-          ],
-        ),
-        bottomNavigationBar: Container(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(),
+          body: TabBarView(
+            children: [
+              phone_nums.PhoneNums(),
+              gallery.Gallery(),
+              calendar.Calendar(),
+            ],
+          ),
+          bottomNavigationBar: Container(
             color: Colors.blue,
             child: TabBar(tabs: const [
               Tab(icon: Icon(Icons.phone)),
               Tab(icon: Icon(Icons.perm_media)),
               Tab(icon: Icon(Icons.contact_page)),
-            ])),
-      ),
-    ));
+            ])
+          ),
+        ),
+      )
+    );
   }
 }
