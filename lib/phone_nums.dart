9b3a7825418ccef3f7395ee08d1d13dbfff3ad11
YinaePark <dlsoso1@unist.ac.kr>
2023-07-04 14:33:49 +0900

    feat: 전화번호부 검색에 contact list 연동

--- lib/phone_nums.dart
+++ lib/phone_nums.dart
@@ -3,7 +3,6 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:fast_contacts/fast_contacts.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-
 import 'package:flutter/foundation.dart';
 
 class PhoneNums extends StatefulWidget {
@@ -26,130 +25,143 @@ class _PhoneNums extends State<PhoneNums> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          title: Text('Contact'),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {
-                showSearch(context: context, delegate: Search());
-              },
-              icon: Icon(Icons.search),)
-          ],
-        ),
-        body: Container(
-          height: double.infinity,
-          child: FutureBuilder(
-            future: getContacts(),
+      home:
+      FutureBuilder(
+        future: getContacts(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(
+              child:
+              SizedBox(height: 50, child: CircularProgressIndicator()),
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              title: Text('Contact'),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  onPressed: !snapshot.hasData ? null : () {
+                    showSearch(context: context, delegate: Search(snapshot.data));
+                  },
+                  icon: Icon(Icons.search),)
+              ],
+            ),
+            body: Container(
+              height: double.infinity,
+              child: FutureBuilder(
+                future: getContacts(),
 
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return const Center(
-                  child:
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return const Center(
+                      child:
                       SizedBox(height: 50, child: CircularProgressIndicator()),
-                );
-              }
-              return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    Contact contact = snapshot.data[index];
-                    return Column(children: [
-                      ListTile(
-                        leading: const CircleAvatar(
-                          radius: 20,
-                          child: Icon(Icons.person),
-                        ),
-                        title: Text(contact.displayName),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text((contact.phones).toString()),
-                          ],
-                        ),
-                        onTap: () async {
-                          await showDialog(
-                              context: context,
-                              builder: (context) {
-                                return Center(
-                                    child: SizedBox(
-                                      height:240,
-                                      child: Scaffold(
-                                        appBar: AppBar(
-                                          leading: const CircleAvatar(
-                                            radius: 20,
-                                            child: Icon(Icons.person),
+                    );
+                  }
+                  return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        Contact contact = snapshot.data[index];
+                        return Column(children: [
+                          ListTile(
+                            leading: const CircleAvatar(
+                              radius: 20,
+                              child: Icon(Icons.person),
+                            ),
+                            title: Text(contact.displayName),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text((contact.phones).toString()),
+                              ],
+                            ),
+                            onTap: () async {
+                              await showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return Center(
+                                      child: SizedBox(
+                                        height:240,
+                                        child: Scaffold(
+                                          appBar: AppBar(
+                                            leading: const CircleAvatar(
+                                              radius: 20,
+                                              child: Icon(Icons.person),
+                                            ),
+                                            title: Text(contact.displayName),
                                           ),
-                                          title: Text(contact.displayName),
-                                        ),
-                                        body: Row(
+                                          body: Row(
                                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                             children: <Widget>[
-                                                MaterialButton(
-                                                  onPressed: () {
-                                                    makePhoneCall((contact.phones).toString());
-                                                    },
-                                                  shape: CircleBorder(),
-                                                  child: Icon(
-                                                    Icons.phone,
-                                                    size: 20,
-                                                  ),
-                                                  color: Colors.greenAccent,
-                                                  textColor: Colors.white,
+                                              MaterialButton(
+                                                onPressed: () {
+                                                  makePhoneCall((contact.phones).toString());
+                                                },
+                                                shape: CircleBorder(),
+                                                child: Icon(
+                                                  Icons.phone,
+                                                  size: 20,
                                                 ),
-                                                MaterialButton(
-                                                  onPressed: () {
-                                                    makeSms((contact.phones).toString());
-                                                  },
-                                                  shape: CircleBorder(),
-                                                  child: Icon(
-                                                    Icons.message,
-                                                    size: 20,
-                                                  ),
-                                                  color: Colors.greenAccent,
-                                                  textColor: Colors.white,
+                                                color: Colors.greenAccent,
+                                                textColor: Colors.white,
+                                              ),
+                                              MaterialButton(
+                                                onPressed: () {
+                                                  makeSms((contact.phones).toString());
+                                                },
+                                                shape: CircleBorder(),
+                                                child: Icon(
+                                                  Icons.message,
+                                                  size: 20,
                                                 ),
-                                                MaterialButton(
-                                                  onPressed: () {},
-                                                  shape: CircleBorder(),
-                                                  child: Icon(
-                                                    Icons.videocam,
-                                                    size: 20,
-                                                  ),
-                                                  color: Colors.greenAccent,
-                                                  textColor: Colors.white,
+                                                color: Colors.greenAccent,
+                                                textColor: Colors.white,
+                                              ),
+                                              MaterialButton(
+                                                onPressed: () {},
+                                                shape: CircleBorder(),
+                                                child: Icon(
+                                                  Icons.videocam,
+                                                  size: 20,
                                                 ),
-                                                MaterialButton(
-                                                  onPressed: () {},
-                                                  shape: CircleBorder(),
-                                                  child: Icon(
-                                                    Icons.info,
-                                                    size: 20,
-                                                  ),
-                                                  color: Colors.greenAccent,
-                                                  textColor: Colors.white,
-                                            ),
-                                          ],
+                                                color: Colors.greenAccent,
+                                                textColor: Colors.white,
+                                              ),
+                                              MaterialButton(
+                                                onPressed: () {},
+                                                shape: CircleBorder(),
+                                                child: Icon(
+                                                  Icons.info,
+                                                  size: 20,
+                                                ),
+                                                color: Colors.greenAccent,
+                                                textColor: Colors.white,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                  );
-                              });
-                        },
-                      ),
-                      const Divider()
-                    ]);
-                  });
-            },
-          ),
-        ),
+                                    );
+                                  });
+                            },
+                          ),
+                          const Divider()
+                        ]);
+                      });
+                },
+              ),
+            ),
+          );
+        }
       ),
     );
   }
+
 
 
 
@@ -208,7 +220,9 @@ class _PhoneNums extends State<PhoneNums> {
 }
 
 
-class Search extends SearchDelegate {
+class Search extends SearchDelegate<Contact> {
+
+  // x 버튼 : 검색 query 초기화
   @override
   List<Widget> buildActions(BuildContext context){
     return <Widget>[
@@ -219,6 +233,8 @@ class Search extends SearchDelegate {
           icon: Icon(Icons.close))
     ];
   }
+
+  // <- 버튼: 창 닫기
   @override
   Widget buildLeading(BuildContext context){
     return IconButton(
@@ -229,15 +245,17 @@ class Search extends SearchDelegate {
     );
   }
 
-
+  // 검색 결과 띄우기(함수 호출 시 전체 덮는 창)
   @override
-  // initillize 핅요
   String selectedResult = '';
   // 최근 query 저장
   List<String> recentList = ["tmp1", "tmp2"];
-  // 전화번호부
-  List<String> listExample = ["elem1", "elem2"];
-
+  // 전화번호부 전체 리스트
+  List<Contact> contact = [];
+  List<Contact> p = [];
+  Search(this.p){
+    contact = p;
+  }
   Widget buildResults(BuildContext context){
     return Container(
       child: Center(
@@ -245,13 +263,16 @@ class Search extends SearchDelegate {
 ,      ),
     );
   }
+
+  // contact 리스트에서 검색해서 반환
   @override
   Widget buildSuggestions(BuildContext context){
-    List<String> suggestionList = [];
+    List<Contact> suggestionList = [];
     query.isEmpty
-        ? suggestionList = recentList
-        : suggestionList.addAll(listExample.where(
-        (element) => element.contains(query),
+        ? suggestionList = contact
+        : suggestionList.addAll(
+          contact.where(
+            (element) => element.displayName.toString().contains(query),
     ));
 
     return ListView.builder(
@@ -259,10 +280,10 @@ class Search extends SearchDelegate {
       itemBuilder: (context, index){
         return ListTile(
           title: Text(
-            suggestionList[index],
+            suggestionList[index].displayName.toString(),
           ),
           onTap: (){
-            selectedResult = suggestionList[index];
+            selectedResult = suggestionList[index].toString();
             showResults(context);
           },
         );
